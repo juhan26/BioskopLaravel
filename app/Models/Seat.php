1282540b96e22c2e757efd9440b5ba38a9a358ab
Juhanda Azril Kamal <cukjuhan@gmail.com>
2024-07-16 11:20:51 +0700

    fix the error (2)

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -10,14 +10,14 @@ class Seat extends Model
     use HasFactory;
     protected $guarded = ['id'];
 
-    
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class); //
+    }
 
   public function isBooked($movie, $dateshowtime)
 {
-  if (!$movie || !$dateshowtime) {
-    return false; // Jika salah satu objek null, return false
-}
-
+  
         return $this->bookings()->where('movie_id', $movie->id)
                                 ->where('dateshowtime_id', $dateshowtime->id)
                                 ->exists();
